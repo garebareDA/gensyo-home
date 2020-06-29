ca68392c18f1aaf9111f091527afa983bc498865
garebare <garedaiki@outlook.jp>
2020-06-30 04:19:28 +0900

    add:iamge

--- src/parts/appbar.tsx
+++ src/parts/appbar.tsx
@@ -1,21 +1,25 @@
 import * as React from 'react';
-
+import { withRouter, RouteComponentProps, } from 'react-router-dom';
 import { AppBar, Toolbar, IconButton, Typography, Drawer, List, ListItem, ListItemText } from '@material-ui/core';
 import { createStyles } from '@material-ui/core/styles';
 import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
-import MenuIcon from '@material-ui/icons/Menu';
-import {
-  withRouter,
-  RouteComponentProps,
-} from 'react-router-dom';
+import { Menu, Home, CloudDownload } from '@material-ui/icons';
 
 const styles = () => createStyles({
   list: {
     width: 200,
+  },
+
+  linkList: {
+    marginLeft: '10%',
+  },
+
+  title: {
+    textAlign: 'right'
   }
 });
 
-interface Props extends RouteComponentProps,WithStyles<typeof styles> { }
+interface Props extends RouteComponentProps, WithStyles<typeof styles> { }
 
 interface State {
   open: boolean,
@@ -40,23 +44,24 @@ class AppBarGensyo extends React.Component<Props, State> {
   render() {
     return (
       <div>
-        <AppBar>
-          <Toolbar>
-            <IconButton onClick={() => this.toggleDrawer(true)}>
-              <MenuIcon />
-            </IconButton>
-            <Typography>
-              原初へ還る
-            </Typography>
-          </Toolbar>
-        </AppBar>
+        <div>
+          <AppBar color={"primary"}>
+            <Toolbar>
+              <IconButton onClick={() => this.toggleDrawer(true)}>
+                <Menu />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        </div>
         <Drawer open={this.state.open} onClose={() => this.toggleDrawer(false)}>
           <List className={this.props.classes.list}>
-            <ListItem button onClick={() => {this.toggleDrawer(false); this.routePush('/')}}>
-              <ListItemText primary="ホーム" />
+            <ListItem button onClick={() => { this.toggleDrawer(false); this.routePush('/') }}>
+              <Home />
+              <ListItemText primary="ホーム" className={this.props.classes.linkList} />
             </ListItem>
-            <ListItem button>
-              <ListItemText primary="配布物" onClick={() => {this.toggleDrawer(false); this.routePush('download')}} />
+            <ListItem button onClick={() => { this.toggleDrawer(false); this.routePush('download') }}>
+              <CloudDownload />
+              <ListItemText className={this.props.classes.linkList} primary="配布物" />
             </ListItem>
           </List>
         </Drawer>
--- src/routers/home.tsx
+++ src/routers/home.tsx
@@ -1,13 +1,48 @@
 import * as React from 'react';
+import { Card, CardMedia, CardContent, Typography, Container } from '@material-ui/core';
+import { createStyles } from '@material-ui/core/styles';
+import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
+import gensyologo from '../../images/gensyologo.png';
 
-class Home extends React.Component {
+const styles = () => createStyles({
+  div: {
+    textAlign: 'center'
+  },
+
+  card: {
+    marginTop: '10vh',
+    textAlign: 'center',
+  },
+
+  image: {
+    height: "30vh",
+  }
+});
+
+interface prop extends WithStyles<typeof styles> { }
+
+class Home extends React.Component<prop> {
   render() {
-    return(
-      <div>
-        <h1>Home</h1>
-      </div>
+    return (
+      <Container className={this.props.classes.div}>
+        <Card className={this.props.classes.card}>
+          <CardMedia
+          className={this.props.classes.image}
+            image="../../images/gensyologo.png"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              原初へ還る
+          </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Discordのサーバーから発足したグループとかそんな感じ
+          </Typography>
+
+          </CardContent>
+        </Card>
+      </Container>
     )
   }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
